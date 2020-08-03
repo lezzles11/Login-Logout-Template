c1bdd244644d0476d42ad2f1e36ba7b526813c79
lesleyc@bu.edu <lesleyc@bu.edu>
2020-08-03 14:52:57 +0800

    able to render home page

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+const hbs = require("express-handlebars");
+var routes = require("./routes");
+var errorHandlers = require("./middleware/errorhandlers");
+var log = require("./middleware/log");
+app.use(log.logger);
+app.engine(
+  "hbs",
+  hbs({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "hbs");
+app.get("/", routes.index);
+app.get("/login", routes.login);
+app.post("/login", routes.loginProcess);
+app.get("/chat", routes.chat);
+app.get("/account/login", routes.login);
+
+app.use(errorHandlers.error);
+app.use(errorHandlers.notFound);
+
+app.listen(3000);
+console.log("App server running on port 3000");
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+module.exports.index = index;
+module.exports.login = login;
+module.exports.loginProcess = loginProcess;
+module.exports.chat = chat;
+function index(req, res) {
+  res.render("index", { title: "Title!" });
+}
+function login(req, res) {
+  res.send("Login");
+}
+function loginProcess(req, res) {
+  res.redirect("/");
+}
+function chat(req, res) {
+  res.send("Chat");
+}
